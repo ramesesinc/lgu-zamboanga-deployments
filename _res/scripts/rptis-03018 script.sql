ee911613d284c9d3fd99a3a65a1def794d87ac1c
Jessie S. Zamora <jzamss@gmail.com>
2020-02-23 12:33:54 +0800

    LANDTAX: clearance: support editing of info (LANDTAX.RECORD_ADMIN) RPTIS: report: faas: custom

---
+++ _res/scripts/rptis-03018 script.sql
@@ -0,0 +1,3 @@
+INSERT INTO sys_usergroup ([objid], [title], [domain], [userclass], [orgclass], [role]) 
+VALUES ('LANDTAX.RECORD_ADMIN', 'LANDTAX RECORD_ADMIN', 'LANDTAX', NULL, NULL, 'RECORD_ADMIN')
+go 
