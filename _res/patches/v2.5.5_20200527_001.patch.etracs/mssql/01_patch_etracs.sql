c143e96e4fee23993a56fec1657ee829fcfb5dfb
Jessie S. Zamora <jzamss@gmail.com>
2020-05-27 11:05:21 +0800

    RPTIS: util: support force delete current faas option CUSTOM: taxclearance: hide division head info

---
+++ _res/patches/v2.5.5_20200527_001.patch.etracs/mssql/01_patch_etracs.sql
@@ -0,0 +1,4 @@
+INSERT INTO sys_var ([name], [value], [description], [datatype], [category]) 
+VALUES ('faas_force_delete_current_record', '1', 'Force delete current faas', 'checkbox', 'ASSESSOR');
+go
+
